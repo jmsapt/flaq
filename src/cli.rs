d556bfc85cf07572ecf383bdf155e01b311241b6
James Appleton <james.appleton01@gmail.com>
2024-05-07 22:21:57 +1000

    functional mvp - query system unimplemented

--- src/cli.rs
+++ src/cli.rs
@@ -17,37 +17,38 @@ pub struct CliArgs {
     /// Clean duplicated fields, where both the tag field and value match.
     ///
     /// Runs after all edits (if used alongside edits).
-    #[clap(long, short, conflicts_with_all = &["clean_all"], action = clap::ArgAction::SetTrue)]
+    #[clap(long, short, conflicts_with_all = &["clean_all"], action)]
     pub clean: bool,
 
-    /// Clean duplicated fields, and deletes non-standard header comments.
+    /// [Caution] Clean duplicated fields, and deletes ALL non-standard header comments.
     ///
-    /// Runs after all edits (if used alongside edits).
-    #[clap(long, short = 'C', conflicts_with_all = &["clean"], action = clap::ArgAction::SetFalse)]
+    /// Runs after all edits (if used alongside edits). If the previous tags are valueable
+    /// do not run this until they have been back up, or ported to standard tags.
+    #[clap(long, conflicts_with_all = &["clean"], action)]
     pub clean_all: bool,
 
     /// [Default] Set field to new given values. Previous values are deleted.
-    #[clap(long, conflicts_with_all = &["append", "delete"], action = clap::ArgAction::SetTrue)]
+    #[clap(long, conflicts_with_all = &["append", "delete"], action, default_value_t = true)]
     pub set: bool,
 
     /// Append flag. If set appends values leaving existing values untouched.
-    #[clap(long, conflicts_with_all = &["set", "delete"], action = clap::ArgAction::SetFalse)]
+    #[clap(long, conflicts_with_all = &["set", "delete"], action)]
     pub append: bool,
 
     /// Deletes associated values for provided fields, leaving the field unset.
-    #[clap(long, conflicts_with_all = &["append", "set"], action = clap::ArgAction::SetFalse)]
+    #[clap(long, conflicts_with_all = &["append", "set"], action)]
     pub delete: bool,
 
     /// Deletes associated values for provided fields, leaving the field unset.
-    #[clap(long, short = 'L', conflicts_with_all = &["list"], action = clap::ArgAction::SetFalse)]
+    #[clap(long, short = 'L', conflicts_with_all = &["list"], action)]
     pub list_detailed: bool,
 
     /// Deletes associated values for provided fields, leaving the field unset.
-    #[clap(long, short = 'l', conflicts_with_all = &["list_detailed"], action = clap::ArgAction::SetFalse)]
+    #[clap(long, short = 'l', conflicts_with_all = &["list_detailed"], action)]
     pub list: bool,
 
     /// Deletes associated values for provided fields, leaving the field unset.
-    #[clap(long, action = clap::ArgAction::SetFalse)]
+    #[clap(long, action)]
     pub dry_run: bool,
 
     #[command(flatten)]
@@ -71,48 +72,81 @@ pub struct Arguments {
 #[derive(Debug, Hash, PartialEq, Eq, Args, Clone)]
 #[clap(next_help_heading = "Tag Fields")]
 pub struct Fields {
+    /// Track/Work name
     #[clap(short, long, num_args(0..))]
     pub title: Option<Vec<String>>,
 
+    /// Recording version (e.g. remix info)
+    ///
+    /// The version field may be used to differentiate multiple versions of the same track
+    /// title in a single collection. (e.g. remix info)
     #[clap(long = "song-version", num_args(0..))]
     pub version: Option<Vec<String>>,
 
+    /// The collection name to which this track belongs
     #[clap(short = 'A', long, num_args(0..))]
     pub album: Option<Vec<String>>,
 
+    /// The track number of this piece if part of a specific larger collection or album
     #[clap(short = 'n', long,num_args(0..))]
     pub tracknumber: Option<Vec<String>>,
 
+    /// The artist generally considered responsible for the work.
+    ///
+    /// In popular music this is usually the performing band or singer. For classical music
+    /// it would be the composer. For an audio book it would be the author of the original text.
     #[clap(short = 'a', long,num_args(0..))]
     pub artist: Option<Vec<String>>,
 
+    /// The artist(s) who performed the work.
+    ///
+    /// In classical music this would be the conductor,
+    /// orchestra, soloists. In an audio book it would be the actor who did the reading. In
+    /// popular music this is typically the same as the ARTIST and is omitted.
     #[clap(long, num_args(0..))]
     pub performer: Option<Vec<String>>,
 
+    /// Copyright attribution, e.g., '2001 Nobody's Band' or '1999 Jack Moffitt'
     #[clap(long, num_args(0..))]
     pub copyright: Option<Vec<String>>,
 
+    /// License information, for example, 'All Rights Reserved'
+    ///
+    /// License information, for example, 'All Rights Reserved', 'Any Use Permitted', a URL to a
+    /// license such as a Creative Commons license (e.g. \"creativecommons.org/licenses/by/4.0/\"),
+    /// or similar.
     #[clap(long, num_args(0..))]
     pub license: Option<Vec<String>>,
 
-    #[clap(long, num_args(0..))]
+    /// Name of the organization producing the track (i.e. the 'record label')
+    #[clap(long, short = 'o', num_args(0..))]
     pub organization: Option<Vec<String>>,
 
+    /// A short text description of the contents
     #[clap(long, num_args(0..))]
     pub description: Option<Vec<String>>,
 
+    /// A short text indication of music genre
     #[clap(short, long, num_args(0..))]
     pub genre: Option<Vec<String>>,
 
+    /// Date the track was recorded
     #[clap(short, long, num_args(0..))]
     pub date: Option<Vec<String>>,
 
+    /// Location where track was recorded
     #[clap(long, num_args(0..))]
     pub location: Option<Vec<String>>,
 
+    /// Contact information for the creators or distributors of the track.
+    ///
+    /// This could be a URL, an email address, the physical address of the producing label.
     #[clap(long, num_args(0..))]
     pub contact: Option<Vec<String>>,
 
+    /// ISRC number for the track
+    ///
+    /// See the ISRC intro page for more information on ISRC numbers.
     #[clap(long, num_args(0..))]
     pub isrc: Option<Vec<String>>,
 }
--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,12 @@
 #![allow(unused)]
 use clap::Parser;
 use cli::{CliArgs, Fields};
+use colored::Colorize;
+use itertools::Itertools;
 use metaflac::block::VorbisComment;
 use metaflac::Tag;
-use std::collections::VecDeque;
+use std::clone;
+use std::collections::{HashSet, VecDeque};
 use std::error::Error;
 use std::option::IntoIter;
 use std::path::{Path, PathBuf};
@@ -39,12 +42,12 @@ fn main() -> Result<(), Box<dyn Error>> {
 
         // apply each edit to every file
         tasks.into_iter().for_each(|(field, tags)| {
-            files.iter_mut().map(|(tag, path)| {
+            files.iter_mut().for_each(|(tag, path)| {
                 let mut meta = tag.vorbis_comments_mut();
                 // Set, append, and delete are parsed as mutually exclusive
-                if args.set {
+                if dbg!(args.set) {
                     /* set */
-                    set_tags(meta, field, tags.to_owned());
+                    set_tags(meta, field, dbg!(tags.to_owned()));
                 } else if args.append {
                     /* append */
                     append_tags(meta, field, tags.to_owned());
@@ -52,22 +55,27 @@ fn main() -> Result<(), Box<dyn Error>> {
                     /* delete */
                     delete_tags(meta, field);
                 }
-
-                if args.clean {
-                    /* clean */
-                    clean_tags(meta);
-                } else if args.clean_all {
-                    /* clean non-standard tags */
-                    clean_non_standard_tags(meta);
-                }
             });
+        });
+
+        files.iter_mut().for_each(|(tag, path)| {
+            let mut meta = tag.vorbis_comments_mut();
+            if args.clean {
+                /* clean */
+                clean_tags(meta);
+            } else if args.clean_all {
+                /* clean non-standard tags */
+                clean_non_standard_tags(meta);
+            }
         });
 
         files.into_iter().try_for_each(|(mut t, p)| {
             if args.list {
                 /* print filenames */
+                list(p.as_path());
             } else if args.list_detailed {
                 /* print filename & all current tags */
+                list_detailed(p.as_path(), t.vorbis_comments_mut());
             }
 
             /* save meta data */
@@ -109,7 +117,19 @@ fn delete_tags(meta: &mut VorbisComment, field: FlacTags) {
 
 /// Removes duplicated tags
 fn clean_tags(meta: &mut VorbisComment) {
-    todo!()
+    let new_values = meta
+        .comments
+        .iter()
+        .map(|(k, v)| {
+            (
+                k.to_owned(),
+                v.iter().map(|s| s.to_owned()).unique().collect::<Vec<_>>(),
+            )
+        })
+        .collect::<Vec<_>>();
+    new_values.into_iter().for_each(|(k, v)| {
+        meta.set(k, v);
+    });
 }
 
 /// Deletes all non-standard tags.
@@ -122,4 +142,19 @@ fn clean_non_standard_tags(meta: &mut VorbisComment) {
         .collect::<Vec<_>>();
 
     bad_tags.into_iter().for_each(|t| meta.remove(&t))
+}
+
+/// Print a newline seperate list of files, ready for piping into other programs.
+fn list(path: &Path) {
+    println!("{}", path.to_str().unwrap())
+}
+
+/// Prints in a more human readable format, listing all files and each tag on that file.
+/// Prints what the edit would be, prior them actually being commit (useful for a dry run).
+fn list_detailed(path: &Path, tag: &VorbisComment) {
+    println!("{:6} {}", "File:".bold(), path.to_str().unwrap().green());
+    println!("{}", "Tags:".bold());
+    tag.comments.keys().sorted().for_each(|k| {
+        println!("{:6}{}:{:?}", "", k.as_str().blue(), tag.comments[k]);
+    });
 }
