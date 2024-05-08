e8af0a4af64e334a8a0b3f877bf596df8004675c
James Appleton <james.appleton01@gmail.com>
2024-05-08 11:30:17 +1000

    ready for packaging

--- build.rs
+++ build.rs
@@ -1,26 +1,35 @@
 #![allow(dead_code)]
+
+use std::{
+    error::Error,
+    fs::{create_dir, create_dir_all},
+    path::PathBuf,
+};
+
+use clap::{CommandFactory, ValueEnum};
+use clap_complete::{generate_to, Shell};
+
 const BINARY_NAME: &str = "flaq";
 include!("src/cli.rs");
 
-fn main() -> Result<(), std::io::Error> {
-    use clap::{CommandFactory, ValueEnum};
-    use clap_complete::{generate_to, Shell};
-    let outdir = match std::env::var_os("OUT_DIR") {
-        None => return Ok(()),
-        Some(outdir) => outdir,
-    };
+fn main() -> Result<(), Box<dyn Error>> {
+    let root_dir = std::env::var_os("CARGO_MANIFEST_DIR").unwrap();
+    let scripts_target = PathBuf::from(root_dir).join("target").join("scripts");
+
+    // create directory
+    create_dir_all(&scripts_target).unwrap();
+
+    println!("cargo:warning=entered");
 
     let mut cmd = CliArgs::command();
     for &shell in Shell::value_variants() {
-        let file = generate_to(shell, &mut cmd, BINARY_NAME, &outdir)?;
+        let file = generate_to(shell, &mut cmd, BINARY_NAME, &scripts_target)?;
 
+        println!("cargo:warning=Autocomplete script generated at {file:?}");
         // source bash autocomplete for testing
         #[cfg(debug_assertions)]
         {
-            if shell == Shell::Bash {
-                println!("source {file:?}");
-                println!("cargo::warning=File {file:?} was source to shell (sources autocompletes for testing)");
-            }
+            println!("cargo:warning=Autocomplete script generated at {file:?}");
         }
     }
 
